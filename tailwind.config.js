51d347e2a0897e07ef31da8a25311a447b789eb2
Yash kolte <yash.kolte9@gmail.com>
2024-05-21 22:00:31 +0530

    Landing Page

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+tailwind.config = {
+  theme: {
+    
+    extend: {
+      animation: {
+        marquee: 'marquee 25s linear infinite',
+        marquee2: 'marquee2 25s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        marquee2: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
+    },
+  },
+  plugins: [],
+}
+}
+
+// module.exports = {
